8f51418563f2e5f5d3c85823371867ccd88ba774
Pablo Arturo Perez Mata <pablo.arturo.perez@gmail.com>
2023-04-19 17:56:28 -0600

    Initial commit

---
+++ src/com/pperezm/Main.java
@@ -0,0 +1,71 @@
+package com.pperezm;
+import java.util.*;
+
+/* Practica de conjuntos
+@author Pablo Arturo Perez Mata
+@mail pablo.arturo.perez@gmail.com
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Character alfabeto = 65;
+        int pares = 0;
+            Set<Character> listaAlfabeto = new HashSet<>();
+            for (int i=0; i<26; i++){
+                listaAlfabeto.add((char) (alfabeto +i));
+            }
+
+            Iterator value = listaAlfabeto.iterator();
+        System.out.println("Los valores de la lista alfabetica son: ");
+        while (value.hasNext()) {
+            System.out.println(value.next());
+        }
+            /*
+       for (Object object : alfabeto) {
+           System.out.println(object);
+        }
+*/
+       SortedSet<Integer> listaPares = new TreeSet<>();
+       pares = 1;
+        while(pares <= 400){
+            if(pares % 2 == 0){
+               listaPares.add(pares);
+            }
+            pares++;
+        }
+
+        Iterator valuePares = listaPares.iterator();
+        System.out.println("Los valores de la lista de pares es: ");
+        while (valuePares.hasNext()) {
+            System.out.println(valuePares.next());
+
+       }
+        int valorInferior = -300;
+        int valorSuperior = 400;
+        SortedSet<Integer> negativos = new TreeSet<>();
+        for (int j=0; j<=valorSuperior; j++)
+        {
+            negativos.add(valorInferior+j);
+        }
+        Iterator valoresNeg = negativos.iterator();
+        System.out.println("Los valores de la lista de negativos a positivos es: ");
+        while (valoresNeg.hasNext()) {
+            System.out.println(valoresNeg.next());
+
+        }
+        listaPares.addAll(negativos);
+        System.out.println("Los pares contienen " + listaPares);
+        System.out.println("Los negativos contienen " + negativos);
+
+        negativos.retainAll(listaPares);
+        System.out.println("Los pares contienen " + listaPares);
+        System.out.println("Los negativos contienen " + negativos);
+        System.out.println("Elementos en negativos entre 20 y 100 " + negativos.subSet(-20,100));
+
+
+
+
+    }
+
+}
